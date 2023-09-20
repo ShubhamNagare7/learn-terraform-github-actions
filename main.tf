1d693f8f41f066d15df1abbbbb63ec45f0e948c3
ShubhamNagare7 <shubham.nagare0777@gmail.com>
2023-09-20 22:41:01 +0530

    My Commit4

--- main.tf
+++ main.tf
@@ -3,17 +3,13 @@
 # My code
 
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "4.52.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "3.4.3"
+  cloud {
+    organization = "HandM-Group1"
+
+    workspaces {
+      name = "workspace-group1"
     }
   }
-  required_version = ">= 1.1.0"
 }
 
 #----------Bucket---Lake---------------
