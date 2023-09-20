332197b083e11e63d74a6f00b57fb399e24a3155
ShubhamNagare7 <shubham.nagare0777@gmail.com>
2023-09-20 22:46:33 +0530

    First Commit22

--- main.tf
+++ main.tf
@@ -1,15 +1,18 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
-# My code
 
 terraform {
-  cloud {
-    organization = "HandM-Group1"
-
-    workspaces {
-      name = "workspace-group1"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.52.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.4.3"
     }
   }
+  required_version = ">= 1.1.0"
 }
 
 #----------Bucket---Lake---------------
